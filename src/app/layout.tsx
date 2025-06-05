d549fb6f790f3ae344d5fdd8e65546f479a926d5
Sidhant Singh Rathore <sidhantsinghrathoreprsnl@gmail.com>
2025-06-05 16:31:05 +0530

    dlt fnt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,17 +6,6 @@ import { Footer } from "@/components/navbar/footer";
 import { ViewTransitions } from "next-view-transitions";
 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "Claircode | Web Design & Development Agency",
   description:
