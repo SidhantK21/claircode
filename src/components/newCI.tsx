aa41193900620a4458584fab773092db62584f25
Sidhant Singh Rathore <sidhantsinghrathoreprsnl@gmail.com>
2025-06-05 16:19:16 +0530

    deps-peer-fix

--- src/components/newCI.tsx
+++ src/components/newCI.tsx
@@ -3,6 +3,7 @@ export const CheckCi=()=>{
         <div>
             Hello i m test for new ci pipeline 
             this is the final test after fixing the build pipeline
+            after fix of legacy-deps 
         </div>
     
     </>
