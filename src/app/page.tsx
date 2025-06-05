d2c66a36710d6f239e7750b05744679e05234645
Sidhant Singh Rathore <sidhantsinghrathoreprsnl@gmail.com>
2025-06-05 16:37:10 +0530

    lyout fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-// import { About } from "@/components/AboutBento";
 import { About } from "@/components/AboutBento";
 import { Container } from "@/components/container";
 import FAQ from "@/components/FAQ";
 import { Hero } from "@/components/Hero";
 import { Pricing } from "@/components/pricing";
 import { Projects } from "@/components/projects";
-import UserStatsCard from "@/components/userProgess";
-import Image from "next/image";
 
 export default function Home() {
   return (
